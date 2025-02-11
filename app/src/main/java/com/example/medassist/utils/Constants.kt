6e5d28ed6e6ac3e097a3d36efbc49e143cee97fe
Madhav <sharmamadhav208@gmail.com>
2025-02-11 23:56:10 +0530

    utils classes for the app

---
+++ app/src/main/java/com/example/medassist/utils/Constants.kt
@@ -0,0 +1,11 @@
+package com.example.medassist.utils
+
+
+class Constants {
+
+    companion object {
+
+        const val BASE_URL = "https://api.spoonacular.com"
+        const val API_KEY = "2aaa2581c56946cdba4adac7e2b6aea8"
+    }
+}
